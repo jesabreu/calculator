fb40fc6abf11f895a561bee20c47c1af3661f945
Jes Abreu <jessabreu834@gmail.com>
2025-01-31 16:24:50 -0500

    initial comment

---
+++ main.py
@@ -0,0 +1,18 @@
+# score = 10
+# if (score > 5) :
+#   print("Nice Score!")
+
+# response = input("Do you agree? (y/N): ")
+# if response.lower() == "y":
+#     print("She said yes!")
+# else:
+#     print("Aw... she said " + response)
+
+print("We are going to add two numbers together!")
+first_number = input("Please enter the first number: ")
+print(first_number)
+second_number = input("Please enter the second number: ")
+print(second_number)
+int(first_number) + int(second_number)
+total = int(first_number) + int(second_number)
+print("The total is", total)
